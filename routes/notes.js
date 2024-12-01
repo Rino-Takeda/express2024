7ef5c5e9696b698d5d548e5e82627b4c936b3a9f
Rino-Takeda <r.takeda.24.7.1531@4nonome.com>
2024-12-01 12:26:39 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
  // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const uri = "xxxxxxxxxxxxxxxxxxxxxxxxxxx.xxx";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
  // データベース、コレクションを指定
