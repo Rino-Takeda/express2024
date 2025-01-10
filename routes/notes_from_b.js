4bb85482071fc0197e9e20748d5797c313bf4c92
Rino-Takeda <r.takeda.24.7.1531@4nonome.com>
2025-01-10 18:35:36 +0900

    change notes_from_b

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,27 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors'); //corsミドルウェアを追加
-
-//corsミドルウェアを使用
-router.use(cors());
-
-// 接続情報を設定
-const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://Lion:Monk22bdbdFS66@test.ffvg9.mongodb.net/?retryWrites=true&w=majority&appName=Test";
-const client = new MongoClient(uri);
-router.get('/', async (req, res) => {
-    // データベース、コレクションを指定
-    const database = client.db('notes');
-    const notes = database.collection('notes');
-    
-    // 全てのドキュメントを取得
-    const note = await notes.find({}).toArray();
-    res.json(note);
-})
-module.exports = router;
-
-/*var express = require('express');
-var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
@@ -45,4 +23,4 @@ const note = await notes.find({}).toArray();
 res.json(note);
 })
 
-module.exports = router;*/
+module.exports = router;
