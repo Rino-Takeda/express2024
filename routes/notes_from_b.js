ffb523a47a0587a968f2f87ad036a55f1e6fd539
Rino-Takeda <r.takeda.24.7.1531@4nonome.com>
2025-01-10 18:30:47 +0900

    add notes_from_b

---
+++ routes/notes_from_b.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); //corsミドルウェアを追加
+
+//corsミドルウェアを使用
+router.use(cors());
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://Lion:Monk22bdbdFS66@test.ffvg9.mongodb.net/?retryWrites=true&w=majority&appName=Test";
+const client = new MongoClient(uri);
+router.get('/', async (req, res) => {
+    // データベース、コレクションを指定
+    const database = client.db('notes');
+    const notes = database.collection('notes');
+    
+    // 全てのドキュメントを取得
+    const note = await notes.find({}).toArray();
+    res.json(note);
+})
+module.exports = router;
+
+/*var express = require('express');
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
+res.json(note);
+})
+
+module.exports = router;*/
