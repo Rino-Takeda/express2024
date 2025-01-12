69e2fe5e7682d9fb0c96576392581e678b39333b
Rino-Takeda <r.takeda.24.7.1531@4nonome.com>
2025-01-12 23:20:29 +0900

    change notes_from_b.js

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI;
+const uri = 'mongodb+srv://Lion:Monk22bdbdFS66@test.ffvg9.mongodb.net/?retryWrites=true&w=majority&appName=Test';
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
